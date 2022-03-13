5b3bf54b8d7e25f3db66ab54526ef8b7f331824f
Jonathasdp <jonathas.dp@gmail.com>
2022-03-12 23:28:27 -0300

    some update

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
@@ -5,10 +6,11 @@ import requests
 from .models import Greeting
 
 # Create your views here.
+
+
 def index(request):
-    r = requests.get('https://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
 
 
 def db(request):
